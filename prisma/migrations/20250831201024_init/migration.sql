5d02889ad4743ff54df9af7548144f330f860f3d
evillem.walesca <evillem.walesca@rastrosystem.com.br>
2025-08-31 19:31:20 -0300

    banco de dados feito

--- prisma/migrations/20250831201024_init/migration.sql
+++ prisma/migrations/20250831201024_init/migration.sql
@@ -1,16 +1,14 @@
--- CreateTable
 CREATE TABLE "Aluno" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "nome" TEXT NOT NULL,
-    "email" TEXT NOT NULL
+    "email" TEXT NOT NULL UNIQUE,
+    "idade" INTEGER 
 );
 
--- CreateTable
+
 CREATE TABLE "Professor" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "nome" TEXT NOT NULL,
-    "materia" TEXT NOT NULL
+    "email" TEXT NOT NULL UNIQUE,
+    "idade" INTEGER
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "Aluno_email_key" ON "Aluno"("email");
